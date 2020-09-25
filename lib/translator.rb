61ef062c74ea8c435ed16f5d88e73304fc2242cd
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-25 21:34:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 # require modules here
 require 'yaml'
 require 'pry'
-path = YAML::load(path.to_yaml)
+path = YAML::load(./lib/emoticons.yml)
 
 def load_library(path)
   final = path.each_with_object({}) do |(key, value), answer|
