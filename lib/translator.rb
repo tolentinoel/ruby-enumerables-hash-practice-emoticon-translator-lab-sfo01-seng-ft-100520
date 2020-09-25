5533ad83dbe0ef3ad3a61bd1cc8411b7c302c285
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-25 23:12:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,8 @@ def load_library(path)
   
   final_answer = file_path.each_with_object({}) do |(key, value), answer|
     answer[key] = value
-    value.each_with_object({}) do |(key, value), icons_set|
-      value
+    value.each_with_object({}) do |(k, v), icons_set|
+      icons_set[v][0]
     end
     end
   final_answer
