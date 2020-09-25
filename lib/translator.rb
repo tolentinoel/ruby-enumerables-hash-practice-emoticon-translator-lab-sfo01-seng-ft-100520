e6d1be081e6ec5685dad35348646ea564d472546
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-25 23:59:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,9 @@ def load_library(path)
   
   hash = file_path.each_with_object({}) do |(key, value), answer|
     answer[key] = value
-    value = 
+    answer[key] = {
+      
+    }
     end
   hash
 binding.pry
