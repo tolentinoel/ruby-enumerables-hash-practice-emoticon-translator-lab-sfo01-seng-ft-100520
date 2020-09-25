008b453acde07c5dbe09b2a55a7076dfdf508246
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-25 21:59:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,18 +6,17 @@ path = YAML::load('./lib/emoticons.yml')
 def load_library(file_path)
   file_path = YAML::load('./lib/emoticons.yml')
 
-  final.each_with_object({}) do |(key, value), emotion|
+  final = final.each_with_object({}) do |(key, value), emotion|
     emotion[key] = value
     emotion[key] = {
       :english => value[0],
       :japanese => value[1]
     }
     end
-  end
   final
-binding.pry
 end
 
+binding.pry
 
 def get_japanese_emoticon
   # code goes here
