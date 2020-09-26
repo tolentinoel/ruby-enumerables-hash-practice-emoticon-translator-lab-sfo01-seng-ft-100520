183bc22049cb41ebf7928d822383b358d7ee8a79
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-26 00:58:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon
   # code goes here
 end
 
-def get_english_meaning(path, emoticon)
+def get_english_meaning(path, emoji)
 result = ""
 emoticon = YAML.load_file('./lib/emoticons.yml')
 
