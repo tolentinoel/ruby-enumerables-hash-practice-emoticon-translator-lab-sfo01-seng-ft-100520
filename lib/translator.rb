bcfa0f4aa84e00dd7dd3f9a7723062d82acd4f46
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-26 00:42:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ end
 def get_english_meaning(path, emoticon)
 result = ""
   load_library(path).each do |key, value|
-    if emoticon == key[value][:japanese]
+    if emoticon == value[:japanese]
       result = key
     else
       result = "Sorry, that emoticon was not found."
