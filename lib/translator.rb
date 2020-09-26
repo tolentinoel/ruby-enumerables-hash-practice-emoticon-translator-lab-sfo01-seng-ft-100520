89b1a2429430591e2864a0b332377dc729e59d41
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-26 00:02:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,15 +6,14 @@ path = YAML.load_file('./lib/emoticons.yml')
 
 def load_library(path)
   file_path = YAML.load_file('./lib/emoticons.yml')
-  
+
   hash = file_path.each_with_object({}) do |(key, value), answer|
     answer[key] = {
-       :english => value[0],
+      :english => value[0],
       :japanese => value[1]
     }
     end
   hash
-binding.pry
 end
 
 def get_japanese_emoticon
