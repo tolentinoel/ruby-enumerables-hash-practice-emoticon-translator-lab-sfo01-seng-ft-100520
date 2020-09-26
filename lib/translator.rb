3329dd0aadcb47bf1ba70b0f130d7f4bd7cd36ee
Ellaine Tolentino <rckrleyn@gmail.com>
2020-09-26 01:01:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,7 @@ emoji = YAML.load_file('./lib/emoticons.yml')
     if emoticon == value[:japanese]
       result = key
     elsif result == ""
-      result = "Sorry, that emoticon was not found."
+      result = "Sorry, that emoticon was not found"
     end
   end
   result
